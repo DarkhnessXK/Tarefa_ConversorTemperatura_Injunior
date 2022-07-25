9d984a61f89f0c0604b803326d3eb3f22b6bd79b
DarkhnessXK <giovannitoledo@id.uff.br>
2022-07-25 20:22:06 -0300

    feat(tarefa2): conversao funcional

---
+++ tarefa_2.js
@@ -0,0 +1,11 @@
+var fahrenheit = 100.0;
+
+function conversor_celsius(F){
+    parseFloat(fahrenheit)
+    celsius = ((fahrenheit - 32)*5) / 9
+    return celsius
+}
+
+console.log("o valor em celsius é ", conversor_celsius(fahrenheit));
+
+//funcional
